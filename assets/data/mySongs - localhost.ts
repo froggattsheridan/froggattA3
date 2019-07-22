f185f1df2566ccdfce467207367c5c5b553aa474
froggattsheridan <52541455+froggattsheridan@users.noreply.github.com>
2019-07-21 20:25:15 -0400

    Add files via upload

--- assets/data/mySongs - localhost.ts
+++ assets/data/mySongs - localhost.ts
@@ -6,27 +6,27 @@ export const MYSONGS: Songs[] = [
     artist: "The Sex Pistols",
     genre: "Punk",
     released: 1979,
-    picture: "/assets/images/myWay.jfif"
+    picture: "./../../assets/images/myWay.jfif"
   },
   {
     name: "One of These Days",
     artist: "Pink Floyd",
     genre: "Progressive Rock",
     released: 1971,
-    picture: "assets/images/oneOfTheseDays.jfif"
+    picture: "./../../assets/images/oneOfTheseDays.jfif"
   },
   {
     name: "Begin the Begin",
     artist: "R. E. M.",
     genre: "Alternative/Indie",
     released: 1986,
-    picture: "./assets/images/beginTheBegin.jfif"
+    picture: "./../../assets/images/beginTheBegin.jfif"
   },
   {
     name: "Fell In Love With A Girl",
     artist: "The White Stripes",
     genre: "Alternative/Indie",
     released: 2001,
-    picture: "/froggattA3/assets/images/fellInLove.jfif"
+    picture: "./../../assets/images/fellInLove.jfif"
   }
 ];
